a5c1419a4cc78008e945aab5ad140c235bb5bc2a
Kacper <kacper-j83@wp.pl>
2024-03-04 23:11:23 +0100

    Added one button

--- app/src/main/java/com/example/zeloman/MainActivity.kt
+++ app/src/main/java/com/example/zeloman/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.zeloman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button1: Button = findViewById(R.id.button)
+        val tekst: TextView = findViewById(R.id.textView)
+        button1.setOnClickListener{
+            tekst.setText(R.string.ratio)
+        }
     }
 }

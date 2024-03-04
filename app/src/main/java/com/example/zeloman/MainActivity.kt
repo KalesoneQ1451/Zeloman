f7389407962fcca97f4aa25a824536f370ff8920
Kacper <kacper-j83@wp.pl>
2024-03-04 23:37:53 +0100

    Added three button

--- app/src/main/java/com/example/zeloman/MainActivity.kt
+++ app/src/main/java/com/example/zeloman/MainActivity.kt
@@ -11,9 +11,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val button1: Button = findViewById(R.id.button)
+        val button2: Button = findViewById(R.id.button2)
+        val button3: Button = findViewById(R.id.button3)
         val tekst: TextView = findViewById(R.id.textView)
         button1.setOnClickListener{
             tekst.setText(R.string.ratio)
         }
+        button2.setOnClickListener{
+            tekst.setText(R.string.ratio2)
+        }
+        button3.setOnClickListener{
+            tekst.setText(R.string.ratio3)
+        }
+
     }
 }
